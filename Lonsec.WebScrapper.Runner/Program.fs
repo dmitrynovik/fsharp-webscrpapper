25f38fc14f080a542542df5062d327fa6ec22f5e
DNovik <DNovik@lonsec.com.au>
2017-03-01 12:31:50 +1100

    Refactoring FrontpageExtractor out

--- Lonsec.WebScrapper.Runner/Program.fs
+++ Lonsec.WebScrapper.Runner/Program.fs
@@ -4,27 +4,14 @@ open System.Text
 open FSharp.Data
 open Lonsec.WebScrapper
 
-
-
 [<EntryPoint>]
 let main argv = 
 
-    let scrappy = new GenericWebScrapper()
-    let url = "http://morningstar.com.au"
-    let outPath = Path.GetTempPath() + "out\\"
-    let newslinkSelector = fun (url:string) -> url.Contains("/article/")
-    let articleRootSelector = ".storyPageHolder"
-
-    let documentLinks = scrappy.loadLinks(url, newslinkSelector)
-
-    // TODO: how do we remove elements like .pagecount - ?
-    documentLinks |> Seq.iter (fun (linkText, href) ->
-        let doc = scrappy.load href
-        let rootContent = doc.CssSelect(articleRootSelector) |> Seq.tryHead
-        if rootContent.IsSome then
-            let writer = new DocumentWriter(scrappy, outPath)
-            writer.scrapDocumentFromLink(linkText, href)
-    )
+    let extractor = new FrontpageExtractor("http://morningstar.com.au", 
+                                    ".storyPageHolder", 
+                                    (fun(url:string) -> url.Contains("/article/")), 
+                                    Path.GetTempPath() + "out\\")
+    extractor.extractAll()
     
     printf "Job done."     
     System.Console.Read() |> ignore
---
+++ Lonsec.WebScrapper/FrontpageExtractor.fs
@@ -0,0 +1,23 @@
+﻿namespace Lonsec.WebScrapper
+
+open FSharp.Data
+
+type FrontpageExtractor(url: string, articleRootSelector: string, 
+                        newslinkSelector: string -> bool, outPath: string) = 
+
+    let scrappy = new GenericWebScrapper()
+
+    member this.extractAll() =
+
+        let documentLinks = scrappy.loadLinks(url, newslinkSelector)
+        let writer = new DocumentWriter(scrappy, outPath)
+
+        // TODO: how do we remove elements like .pagecount - ?
+        documentLinks |> Seq.iter (fun (linkText, href) ->
+            let doc = scrappy.load href
+            let rootContent = doc.CssSelect(articleRootSelector) |> Seq.tryHead
+            if rootContent.IsSome then
+                writer.scrapDocumentFromLink(linkText, href)
+        )
+
+
