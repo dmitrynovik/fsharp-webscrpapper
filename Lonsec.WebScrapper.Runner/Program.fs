5f534bc7e4e31cf8225169a0e426e893145311e5
DNovik <DNovik@lonsec.com.au>
2017-03-03 11:50:39 +1100

    Supporting moneymanagement.com.au

--- Lonsec.WebScrapper.Runner/Program.fs
+++ Lonsec.WebScrapper.Runner/Program.fs
@@ -1,4 +1,6 @@
-﻿open Lonsec.WebScrapper
+﻿open System
+open System.Diagnostics
+open Lonsec.WebScrapper
 
 [<EntryPoint>]
 let main argv = 
@@ -6,7 +8,10 @@ let main argv =
     Lonsec.WebScrapper.Runner.SiteList.all |> Seq.iter (fun siteSettings -> 
         printf "processing %s\n" siteSettings.url 
         let extractor = new FrontpageExtractor(siteSettings)
-        extractor.extractAll()
+        try
+            extractor.extractAll()
+        with
+            | err -> Trace.TraceError (err.ToString())
     )
     
     printf "\n\nJob done."
--- Lonsec.WebScrapper.Runner/SiteList.fs
+++ Lonsec.WebScrapper.Runner/SiteList.fs
@@ -6,41 +6,41 @@ open Lonsec.WebScrapper
 module SiteList =
     
     let all = [|
-//        { 
-//            url = "http://morningstar.com.au";
-//            newslinkSelector = (fun(url:string) -> url.Contains("/article/"));
-//            articleRootSelectors = [|".storyPageHolder"|];
-//            outPath = Path.GetTempPath() + "scrappy\\Morningstar\\";
-//            categories = [];
-//        };
-//        { 
-//            url = "http://morningstar.com.au/Funds";
-//            newslinkSelector = (fun(url:string) -> url.Contains("/article/"));
-//            articleRootSelectors = [|".storyPageHolder"|];
-//            outPath = Path.GetTempPath() + "scrappy\\Morningstar\\Funds\\";
-//            categories = [{ name = "Funds"; weight = 1.0 }];
-//        };
-//        { 
-//            url = "http://moneymanagement.com.au/news";
-//            newslinkSelector = (fun(url:string) -> url.Contains("/news/"));
-//            articleRootSelectors = [|".cm-article-body"; ".News"|];
-//            outPath = Path.GetTempPath() + "scrappy\\Moneymanagement\\";
-//            categories = [];
-//        };
-//        { 
-//            url = "http://moneymanagement.com.au/funds-management";
-//            newslinkSelector = (fun(url:string) -> url.Contains("/news/"));
-//            articleRootSelectors = [|".cm-article-body"; ".News"|];
-//            outPath = Path.GetTempPath() + "scrappy\\Moneymanagement\\Funds\\";
-//            categories = [{ name = "Funds"; weight = 1.0 }];
-//        };
-//        { 
-//            url = "http://moneymanagement.com.au/news/financial-planning";
-//            newslinkSelector = (fun(url:string) -> url.Contains("/news/"));
-//            articleRootSelectors = [|".cm-article-body"; ".News"|];
-//            outPath = Path.GetTempPath() + "scrappy\\Moneymanagement\\FinancialPlanning\\";
-//            categories = [{ name = "FinancialPlanning"; weight = 1.0 }];
-//        };
+        { 
+            url = "http://morningstar.com.au";
+            newslinkSelector = (fun(url:string) -> url.Contains("/article/"));
+            articleRootSelectors = [|".storyPageHolder"|];
+            outPath = Path.GetTempPath() + "scrappy\\Morningstar\\";
+            categories = [];
+        };
+        { 
+            url = "http://morningstar.com.au/Funds";
+            newslinkSelector = (fun(url:string) -> url.Contains("/article/"));
+            articleRootSelectors = [|".storyPageHolder"|];
+            outPath = Path.GetTempPath() + "scrappy\\Morningstar\\Funds\\";
+            categories = [{ name = "Funds"; weight = 1.0 }];
+        };
+        { 
+            url = "http://moneymanagement.com.au/news";
+            newslinkSelector = (fun(url:string) -> url.Contains("/news/"));
+            articleRootSelectors = [|".cm-article-body"; ".News"|];
+            outPath = Path.GetTempPath() + "scrappy\\Moneymanagement\\";
+            categories = [];
+        };
+        { 
+            url = "http://moneymanagement.com.au/funds-management";
+            newslinkSelector = (fun(url:string) -> url.Contains("/news/"));
+            articleRootSelectors = [|".cm-article-body"; ".News"|];
+            outPath = Path.GetTempPath() + "scrappy\\Moneymanagement\\Funds\\";
+            categories = [{ name = "Funds"; weight = 1.0 }];
+        };
+        { 
+            url = "http://moneymanagement.com.au/news/financial-planning";
+            newslinkSelector = (fun(url:string) -> url.Contains("/news/"));
+            articleRootSelectors = [|".cm-article-body"; ".News"|];
+            outPath = Path.GetTempPath() + "scrappy\\Moneymanagement\\FinancialPlanning\\";
+            categories = [{ name = "FinancialPlanning"; weight = 1.0 }];
+        };
         { 
             url = "http://moneymanagement.com.au/news/superannuation";
             newslinkSelector = (fun(url:string) -> url.Contains("/news/"));
@@ -48,12 +48,12 @@ module SiteList =
             outPath = Path.GetTempPath() + "scrappy\\Moneymanagement\\Superannuation\\";
             categories = [{ name = "Superannuation"; weight = 1.0 }];
         };
-//        { 
-//            url = "http://moneymanagement.com.au/news/people-products";
-//            newslinkSelector = (fun(url:string) -> url.Contains("/news/"));
-//            articleRootSelectors = [|".cm-article-body"; ".News"|];
-//            outPath = Path.GetTempPath() + "scrappy\\Moneymanagement\\PeopleProducts\\";
-//            categories = [{ name = "PeopleProducts"; weight = 1.0 }];
-//        };
+        { 
+            url = "http://moneymanagement.com.au/news/people-products";
+            newslinkSelector = (fun(url:string) -> url.Contains("/news/"));
+            articleRootSelectors = [|".cm-article-body"; ".News"|];
+            outPath = Path.GetTempPath() + "scrappy\\Moneymanagement\\PeopleProducts\\";
+            categories = [{ name = "PeopleProducts"; weight = 1.0 }];
+        };
     |]
 
